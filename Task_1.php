3ad7d166d3f10b55f100a7cc4c8527e2f30a547a
panha-hok <79347912+panha-hok@users.noreply.github.com>
2021-05-14 23:09:38 +0700

    [TASK] Week 4 assigNment

--- Task_1.php
+++ Task_1.php
@@ -1,4 +1,25 @@
 <?php
-$string = "PHP ot emocleW";
-echo " ".strrev($string);
+class Car {
+  public $name;
+  public $color;
+  public function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color; 
+  }
+  public function intro() {
+    echo "The car is {$this->name} and the color is {$this->color}."; 
+  }
+}
+
+
+class Toyota extends Car {
+  public function message() {
+    echo "Am I a car or a motorbike? "; 
+  }
+}
+
+$toyota = new Toyota("Toyota", "blue");
+$toyota->message();
+$toyota->intro();
 ?>
+ 
